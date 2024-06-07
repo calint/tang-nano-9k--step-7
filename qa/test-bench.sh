da3f9bef67c382157db0972623fc76920a45a8df
calint <calin.tenitchi@gmail.com>
2024-06-07 19:35:12 +0200

    .

--- qa/test-bench.sh
+++ qa/test-bench.sh
@@ -6,7 +6,7 @@ set -e
 cd $(dirname "$0")
 
 SRCPTH=../../src
-LIBPTH=/home/c/.wine/drive_c/Gowin/Gowin_V1.9.9.03_x64/IDE/simlib/gw1n/prim_tsim.v
+# LIBPTH=/home/c/.wine/drive_c/Gowin/Gowin_V1.9.9.03_x64/IDE/simlib/gw1n/prim_tsim.v
 
 cd $1
 pwd
@@ -17,9 +17,7 @@ pwd
 iverilog -Winfloop -pfileline=1 -o iverilog.vvp TestBench.v \
     $SRCPTH/BESDPB.v \
     $SRCPTH/Cache.v \
-    $SRCPTH/BurstRAM.v \
-    $SRCPTH/gowin_rpll/gowin_rpll.v \
-    $LIBPTH
+    $SRCPTH/BurstRAM.v
 
 vvp iverilog.vvp
 rm iverilog.vvp
